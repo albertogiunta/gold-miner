8e44d4fbe8c49e54c6c02757853c5db3d96e4872
Bilyana Georgieva <bibby.georgieva@gmail.com>
2019-04-13 10:10:18 +0200

    Delete commented code

--- GoldMiner/GameScene.swift
+++ GoldMiner/GameScene.swift
@@ -26,22 +26,6 @@ class GameScene: SKScene {
   }
 
   func setup() {
-//    let cityWidth: CGFloat = (self.screenBounds.width - 40.0)
-//    let cityHeight: CGFloat = cityWidth * 9.0 / 16.0
-//    let cityX: CGFloat = 20
-//    let cityY: CGFloat = 0//cityHeight / 2.0
-//    let cityRect = CGRect(origin: CGPoint(x: cityX, y: cityY), size: CGSize(width: cityWidth, height: cityHeight))
-//
-//    self.top = SKShapeNode(ellipseIn: cityRect)
-//    self.addChild(self.top)
-//
-//    self.bottom = SKShapeNode(rect: CGRect(x: 0, y: 0, width: self.screenBounds.width, height: cityHeight/2.0))
-//    self.addChild(self.bottom)
-//
-//    self.label = SKLabelNode(text: "Score: \(score)")
-//    self.label.position = CGPoint(x: self.screenBounds.width / 2, y: self.screenBounds.height - 20 - 44)
-//    self.addChild(self.label)
-
     self.bottom = SKShapeNode(rect: CGRect(x: 0, y: 0, width: self.screenBounds.width, height: self.screenBounds.height/3 * 2))
     self.addChild(self.bottom)
 
@@ -57,37 +41,7 @@ class GameScene: SKScene {
 
   }
 
-  //  func createAsteroid() {
-  //    let asteroid = SKShapeNode(circleOfRadius: CGFloat.random(in: 10..<50))
-  //    asteroid.fillColor = .gray
-  //    asteroid.strokeColor = .gray
-  //    asteroid.position = CGPoint(x: CGFloat.random(in: 0..<self.screenBounds.width), y: self.screenBounds.height)
-  //    let cityCenter = CGPoint(x: self.frame.width / 2.0, y: self.city.frame.height / 2.0)
-  //    let roadToCollisionAction = SKAction.move(to: cityCenter, duration: 4)
-  //    self.addChild(asteroid)
-  //    self.asteroids.append(asteroid)
-  //    asteroid.run(roadToCollisionAction) {
-  //      self.removeChildren(in: [asteroid])
-  //    }
-  //  }
-
   override func update(_ currentTime: TimeInterval) {
-    // Called before each frame is rendered
-    //    guard let lastAsterodiTime = self.lastAsterodiTime else {
-    //      spawnAsteroid(currentTime: currentTime)
-    //      return
-    //    }
-    //
-    //    if currentTime - lastAsterodiTime > 1 {
-    //      spawnAsteroid(currentTime: currentTime)
-    //    }
-    //
-    //    self.label.text = "Score: \(self.score)"
   }
-
-  //  func spawnAsteroid(currentTime: TimeInterval) {
-  //    self.createAsteroid()
-  //    self.lastAsterodiTime = currentTime
-  //  }
 }
 
